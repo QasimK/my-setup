995008e3f9ee68223a3ef7d4491f120e20fc2d9e
QasimK <QasimK@users.noreply.github.com>
2017-06-23 19:46:39 +0000

    Add download script

---
+++ gogo.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Download dot files into pwd
+URL=https://raw.githubusercontent.com/QasimK/my-setup/master
+declare -a FILES=('.inputrc' '.vimrc' '.tmux.conf' '.gitignore')
+for FILE in "${FILES[@]}"; do
+    wget -q --backups=1 $URL/$FILE
+done
+
