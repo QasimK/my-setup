c467222ec241aed0062aa3cc09623ec9cc6eb1ad
Qasim K <QasimK@users.noreply.github.com>
2018-02-23 15:40:18 +0000

    Update gogo.sh

--- gogo.sh
+++ gogo.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # Download dot files into pwd
 URL=https://raw.githubusercontent.com/QasimK/my-setup/master
-declare -a FILES=('.inputrc' '.vimrc' '.tmux.conf' '.gitignore' '.pdbrc')
+declare -a FILES=('.inputrc' '.vimrc' '.tmux.conf' '.gitignore' '.pdbrc' '.pypirc')
 for FILE in "${FILES[@]}"; do
     wget -q --backups=1 $URL/$FILE
 done
-
